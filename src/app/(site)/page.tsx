5af7fb5453b772e8650c27a9cc26a66809f76a76
Cesar Faber <cesarolvr@Cesars-MacBook-Air.local>
2024-07-11 19:10:48 -0300

    first commit

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
+
+export default function Home() {
+	return (
+		<main>
+			<div className="bg-[#0D121F] px-[100px] text-white">
+				<Navbar />
+				<Header />
+			</div>
+		</main>
+	);
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+
+// Styles
+import "./globals.css";
+
+const font = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AI2SaaS",
+  description: "Create your own AI SaaS",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>{children}</UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
