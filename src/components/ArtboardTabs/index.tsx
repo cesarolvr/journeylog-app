01f3e02235139b080819d652393dde732002f1f2
Cesar Faber <cesarolvr@Cesars-MacBook-Air.local>
2024-08-15 19:32:21 -0300

    fixing click on exceeded tab

--- src/components/ArtboardTabs/index.tsx
+++ src/components/ArtboardTabs/index.tsx
@@ -24,8 +24,6 @@ const ArtboardTabs = ({
   const firsTabs = isMobile ? journeyTabs.slice(0, 2) : journeyTabs.slice(0, 3);
   const lastTabs = isMobile ? journeyTabs.slice(2) : journeyTabs.slice(3);
 
-  console.log(lastTabs);
-
   return (
     <NavbarItem className="justify-center flex items-center">
       {journeyTabs.length > 0 && (
@@ -38,16 +36,26 @@ const ArtboardTabs = ({
           id="tabs"
         >
           {(item: any) => (
-            <Tab key={item.id} id="tabsButton" title={item.name} className="flex items-center align-middle">
+            <Tab
+              key={item.id}
+              id="tabsButton"
+              title={item.name}
+              className="flex items-center align-middle"
+            >
               <Dropdown className="p-0 h-full">
                 <DropdownTrigger className="p-0 h-full" id="tabsPlus">
-                  <div className="border-0 rounded-lg p-0 px-3 py-2 pl-6 ml-[-20px] flex align-middle items-center border-[#373737] bg-[#373737] cursor-pointer	 relative z-10 rounded-l-none border-l-0 h-full">+{lastTabs.length}</div>
+                  <div className="border-0 rounded-lg p-0 px-3 py-2 pl-6 ml-[-20px] flex align-middle items-center border-[#373737] bg-[#373737] cursor-pointer	 relative z-10 rounded-l-none border-l-0 h-full">
+                    +{lastTabs.length}
+                  </div>
                 </DropdownTrigger>
                 <DropdownMenu aria-label="Static Actions">
-                  {lastTabs.map(({ name, id }: any) => {
+                  {lastTabs.map((item: any) => {
                     return (
-                      <DropdownItem key={id} onClick={(f) => f}>
-                        {name}
+                      <DropdownItem
+                        key={item.id}
+                        onClick={(e) => handleTabSelection(item.id)}
+                      >
+                        {item.name}
                       </DropdownItem>
                     );
                   })}
