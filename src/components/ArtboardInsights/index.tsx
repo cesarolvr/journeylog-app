bff25a7692ea24cf687dc8be4a6a4204e790baa1
Cesar Oliveira <cesar2012oliveira@gmail.com>
2024-10-02 18:27:12 -0300

    github calendar working

--- src/components/ArtboardInsights/index.tsx
+++ src/components/ArtboardInsights/index.tsx
@@ -2,10 +2,15 @@
 
 import { Select, SelectItem } from "@nextui-org/react";
 import classNames from "classnames";
-import Calendar from "react-github-contribution-calendar";
 import { Share, X } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { DateTime } from "luxon";
+
+import CalHeatmap from "cal-heatmap";
+import Tooltip from "cal-heatmap/plugins/Tooltip";
+import CalendarLabel from "cal-heatmap/plugins/CalendarLabel";
+
+import "cal-heatmap/cal-heatmap.css";
 
 const ArtboardInsights = ({
   isInsightsOpened,
@@ -13,31 +18,94 @@ const ArtboardInsights = ({
   getInsights,
   activeTab,
 }: any) => {
-  const [frequency, setFrequency] = useState({});
+  const [frequency, setFrequency] = useState([]);
 
   useEffect(() => {
     const triggerGetInsights = async () => {
       const res = await getInsights(2024, activeTab?.id);
-      let object: any = {};
 
-      res.forEach((item: any) => {
-        object[item.created_at.split("T")[0]] = Math.round(
-          (item?.content?.length || 0) / 150
-        );
+      const newRes = res.map((item: any) => {
+        return {
+          date: item.created_at.split("T")[0],
+          value: Math.round((item?.content?.length || 0) / 150),
+        };
       });
-      setFrequency(object);
+      setFrequency(newRes);
     };
     if (isInsightsOpened) {
       triggerGetInsights();
     }
   }, [isInsightsOpened]);
 
-  console.log(frequency);
-
   const today = DateTime.now().toUTC().toJSDate();
   const monthWithPad = `0${today.getMonth() + 1}`.slice(-2);
   const dayWithPad = `0${today.getDate()}`.slice(-2);
   const initialDateSelected = `${today.getFullYear()}-${monthWithPad}-${dayWithPad}`;
+
+  useEffect(() => {
+    if (frequency.length === 0) return;
+    const cal = new CalHeatmap();
+    cal.paint(
+      {
+        data: {
+          source: frequency,
+          type: "json",
+          x: "date",
+          y: "value",
+        },
+        date: { start: new Date("2024-01-01"), max: new Date("2024-10-15") },
+        range: 12,
+        theme: "dark",
+        scale: {
+          color: {
+            type: "threshold",
+            range: ["#626262", "#515e54", "#4d6551", "#468A51", "#39D353"],
+            domain: [2, 4, 6, 8, 10],
+          },
+        },
+        domain: {
+          type: "month",
+          gutter: 4,
+          label: { text: "MMM", textAlign: "start", position: "top" },
+        },
+        subDomain: {
+          type: "ghDay",
+          radius: 2,
+          width: 27,
+          height: 27,
+          gutter: 5,
+        },
+      },
+      [
+        [
+          Tooltip,
+          {
+            text: function (date: any, value: any, dayjsDate: any) {
+              return (
+                (value ? value : "No") +
+                " logs on " +
+                dayjsDate.format("dddd, MMMM D, YYYY")
+              );
+            },
+          },
+        ],
+        [
+          CalendarLabel,
+          {
+            width: 35,
+            textAlign: "end",
+            text: () => {
+              return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
+                (d, i) => d
+              );
+            },
+
+            padding: [25, 10, 0, 0],
+          },
+        ],
+      ]
+    );
+  }, [frequency]);
 
   return (
     <div className="relative">
@@ -71,36 +139,36 @@ const ArtboardInsights = ({
 
       <div
         className={classNames(
-          "fixed w-[550px] max-w-[85%] h-full bg-[#1E1E1E] border-l-1 py-10 px-7 overflow-scroll border-[#303030] right-0 top-0 z-[902]",
+          "fixed w-[550px] max-w-[85%] h-full bg-[#1E1E1E] border-l-1 py-10 overflow-scroll border-[#303030] right-0 top-0 z-[902]",
           {
             "right-0": isInsightsOpened,
             "right-[-600px]": !isInsightsOpened,
           }
         )}
       >
-        <div className="flex justify-between">
+        <div className="flex justify-between px-7">
           <p className="mb-3">Insights</p>
           <Share className="opacity-25 cursor-not-allowed" />
         </div>
 
-        <div>
+        <div className="px-7">
           <ul className="flex justify-start">
             <li className="flex justify-center items-center flex-col p-4 py-8 pl-0 text-[#fff]">
               <p className="text-5xl font-bold">22</p>
-              <span>Days streak</span>
+              <span>Days in a row</span>
+            </li>
+            <li className="flex justify-center items-center flex-col p-4 py-8 text-[#5C5C5C]">
+              <p className="text-5xl font-bold">{frequency.length}</p>
+              <span>Days with logs</span>
             </li>
             <li className="flex justify-center items-center flex-col p-4 py-8 text-[#5C5C5C]">
               <p className="text-5xl font-bold">54</p>
-              <span>Journey's days</span>
-            </li>
-            <li className="flex justify-center items-center flex-col p-4 py-8 text-[#5C5C5C]">
-              <p className="text-5xl font-bold">Tue</p>
-              <span>Busiest days</span>
+              <span>Total days</span>
             </li>
           </ul>
         </div>
-        <div className="pb-8">
-          <div className="flex justify-between mb-6 items-center">
+        <div className="">
+          <div className="flex justify-between mb-3 px-7 items-center">
             <p>Frequency</p>
             <Select
               isDisabled={true}
@@ -111,28 +179,14 @@ const ArtboardInsights = ({
               {(item) => <SelectItem key={item.key}>{item.label}</SelectItem>}
             </Select>
           </div>
-          <Calendar
-            values={frequency}
-            until={initialDateSelected}
-            weekNames={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
-            panelColors={[
-              "#3E3E3E",
-              "#5C5B5B",
-              "#434F45",
-              "#3E5642",
-              "#468A51",
-              "#39D353",
-            ]}
-            panelAttributes={
-              {
-                // ry: 3,
-                // width: 15,
-                // height: 15
-              }
-            }
-          />
+          <div className="w-full overflow-scroll pl-7 pr-8 mb-9">
+            <div className="flex">
+              <div id="cal-heatmap" className="mr-6 flex-shrink-0"></div>
+              <div className="w-[50px] h-[200px] flex-shrink-0"></div>
+            </div>
+          </div>
         </div>
-        <div className="mb-10">
+        <div className="mb-10 px-7">
           <div className="flex justify-between mb-6 items-center">
             <p>Habit consistency</p>
             <span className="text-[#656565]">Last 30 days</span>
@@ -141,7 +195,7 @@ const ArtboardInsights = ({
             Soon...
           </div>
         </div>
-        <div>
+        <div className="px-7">
           <div className="flex justify-between mb-6 items-center">
             <p>Habit density</p>
             <span className="text-[#656565]">Last 7 days</span>
