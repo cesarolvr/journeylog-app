04659911bf3fac9f4776bb0e4642dbc7d2dc78bc
Cesar Oliveira <cesar2012oliveira@gmail.com>
2024-10-08 17:05:34 -0300

    fixing purpose screen

--- src/app/(site)/purpose/page.tsx
+++ src/app/(site)/purpose/page.tsx
@@ -13,23 +13,21 @@ const reenie = Reenie_Beanie({ subsets: ["latin"], weight: "400" });
 const Purpose = () => {
   return (
     <div className="p-6 flex justify-center items-start w-[100svw] h-[100svh] bg-[#171717]">
-      <Link className="fixed top-[30px] left-[30px] flex" href="/">
+      <Link className="fixed top-[30px] left-[20px] flex" href="/">
         <ChevronLeft />
         Back to home
       </Link>
       <div className="w-full max-w-[100%] flex-col auth flex items-center justify-center">
         <p
-          className={`md:inline-flex hidden text-white ${reenie.className} text-[35px] mt-10 mb-12 flex`}
+          className={`text-white ${reenie.className} text-[50px] mt-24 mb-8 flex`}
         >
           Journeylog
-          <Image
-            src={LOGO}
-            alt=""
-            className="mt-[-25px] w-[40px] ml-3"
-          />
+          <Image src={LOGO} alt="" className="mt-[-25px] w-[40px] ml-3" />
         </p>
-        <div className="w-[600px] max-w-[95%] text-center mt-20 md:mt-0">
-          <h1 className="md:text-[65px] text-[40px] leading-[40px] font-bold mb-5 md:mb-10 text-[white]">Our purpose</h1>
+        <div className="w-[600px] max-w-[95%] text-center mt-5 md:mt-0">
+          <h1 className="md:text-[65px] text-[40px] leading-[40px] font-bold mb-5 md:mb-10 text-[white]">
+            Our purpose
+          </h1>
           <p className="text-[16px] md:text-[24px] leading-[25px] md:leading-[35px] mb-7 text-[#909090]">
             "I was having a lot of difficulty maintaining consistency at the
             gym. I started logging the times I went, and the frequency view in
@@ -47,11 +45,15 @@ const Purpose = () => {
             started logging the times I went, and the frequency view in
             Journeylog became a motivator. Amazing app!"
           </p>
-          <p className="text-[16px] md:text-[24px] leading-[25px] md:leading-[35px] mb-28 text-[#909090]">
+          <p className="text-[16px] md:text-[24px] leading-[25px] md:leading-[35px] mb-12 text-[#909090]">
             I was having a lot of difficulty maintaining consistency at the gym.
             I started logging the times I went, and the frequency view in
             Journeylog became a motivator.
           </p>
+          <Link className="flex mb-28 text-center w-full items-center justify-center text-[#b7b7b7]" href="/">
+            <ChevronLeft />
+            Back to home
+          </Link>
         </div>
       </div>
     </div>
